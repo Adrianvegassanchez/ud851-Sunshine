c456c241d5154f31267785efe3651e60c8c5b216
adrian.vegas <adrian.vegas@bbva.com>
2018-01-03 12:37:32 +0100

    Lesson 6 - Shared Preferences & Lesson 7 - SQLite Completed

---
+++ S06.02-Exercise-SettingsFragment/app/src/main/java/com/example/android/sunshine/SettingsFragment.java
@@ -0,0 +1,69 @@
+package com.example.android.sunshine;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+
+public class SettingsFragment extends PreferenceFragmentCompat implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private void setPreferenceSummary(Preference preference, Object value) {
+        String stringValue = value.toString();
+        String key = preference.getKey();
+
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            preference.setSummary(stringValue);
+        }
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        addPreferencesFromResource(R.xml.pref_general);
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        int count = prefScreen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+            Preference p = prefScreen.getPreference(i);
+            if (!(p instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(p.getKey(), "");
+                setPreferenceSummary(p, value);
+            }
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        /* Register the preference change listener */
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if (null != preference) {
+            if (!(preference instanceof CheckBoxPreference)) {
+                setPreferenceSummary(preference, sharedPreferences.getString(key, ""));
+            }
+        }
+    }
+}
--- S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
+++ S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
@@ -15,19 +15,51 @@
  */
 package com.example.android.sunshine.data;
 
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Manages a local database for weather data.
  */
-// TODO (11) Extend SQLiteOpenHelper from WeatherDbHelper
-public class WeatherDbHelper {
+// TODODONE (11) Extend SQLiteOpenHelper from WeatherDbHelper
+public class WeatherDbHelper extends SQLiteOpenHelper{
 
-//  TODO (12) Create a public static final String called DATABASE_NAME with value "weather.db"
+    //  TODODONE (12) Create a public static final String called DATABASE_NAME with value "weather.db"
+    public static final String DATABASE_NAME = "weather.db";
+    //  TODODONE (13) Create a private static final int called DATABASE_VERSION and set it to 1
+    public static final int DATABASE_VERSION = 1;
 
-//  TODO (13) Create a private static final int called DATABASE_VERSION and set it to 1
+    //  TODODONE (14) Create a constructor that accepts a context and call through to the superclass constructor
+    public WeatherDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
 
-//  TODO (14) Create a constructor that accepts a context and call through to the superclass constructor
+    //  TODODONE (15) Override onCreate and create the weather table from within it
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_WAITLIST_TABLE = "CREATE TABLE " + WeatherContract.WeatherEntry.TABLE_NAME + " (" +
+                WeatherContract.WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                WeatherContract.WeatherEntry.COLUMN_DEGREES     + " REAL, " +
+                WeatherContract.WeatherEntry.COLUMN_DATE        + " INTEGER," +
+                WeatherContract.WeatherEntry.COLUMN_HUMIDITY    + " REAL, " +
+                WeatherContract.WeatherEntry.COLUMN_MAX_TEMP    + " REAL, " +
+                WeatherContract.WeatherEntry.COLUMN_MIN_TEMP    + " REAL, " +
+                WeatherContract.WeatherEntry.COLUMN_PRESSURE    + " REAL, " +
+                WeatherContract.WeatherEntry.COLUMN_WEATHER_ID  + " INTEGER, " +
+                WeatherContract.WeatherEntry.COLUMN_WIND_SPEED  + " REAL" +
+                "); ";
 
-//  TODO (15) Override onCreate and create the weather table from within it
+        db.execSQL(SQL_CREATE_WAITLIST_TABLE);
+    }
 
-//  TODO (16) Override onUpgrade, but don't do anything within it yet
+
+    //  TODODONE (16) Override onUpgrade, but don't do anything within it yet
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
 }
